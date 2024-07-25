b98a42d45f19a0274e7040ba253b6f9c2a562710
Talgat Tokayev <ttokayev7@gmail.com>
2024-07-26 00:23:35 +0500

    header

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import './index.html';
 import './index.scss';
+
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav');
+const body = document.body
+
+if (hamburger && nav) {
+	hamburger.addEventListener('click', () => {
+		hamburger.classList.toggle('_active');
+		nav.classList.toggle('_active');
+		body.classList.toggle('_lock');
+	})
+}
